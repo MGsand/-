c3b477c166ed8ca2fff7cd8e2517e81e63b4f82b
MGsand <60767172+MGsand@users.noreply.github.com>
2020-05-29 22:19:45 +0700

    Сортировка слов из A.txt файла
    Теперь-то точно все, спс что не остановил

--- kursovikkk.cpp
+++ kursovikkk.cpp
@@ -1,121 +1,134 @@
-
-#include <iostream> // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ cout
 #include <stdio.h>
-#include <string.h> // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
-using namespace std; // пїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ std
-
-int ii = 0; //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ q
-
-void Shellsort(char* A, int N)
-{
-    int h = 2, i, j, k;
-    char temp, t;
-    for (k = h; k > 0; k--) {
-        for (i = k; i < N; i++) {
-            t = A[i];
-            j = i - k;
-
-            while (j >= 0 && t < A[j]) {
-                temp = A[j + k];
-                A[j + k] = A[j];
-                A[j] = temp;
-                j = j - k;
-            }
-            t = A[j + k];
-        }
-    }
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+char **A;
+int j=0;
+typedef char *pint;
+   
+void Shellsort(char **AA,int N){
+	int h=2,i,jj,k;
+	char t[20],temp[20];//slovo
+	for(k=h;k>0;k--){
+	 for(i=k;i<N;i++){
+	 	strcpy(t,AA[i]);
+	 	jj=i-k;
+	 
+	 	while(jj>=0 && strcmp(t,AA[jj]) < 0){ //sortirovka
+	 	
+	 		strcpy(temp,AA[jj+k]);
+	 		strcpy(AA[jj+k],AA[jj]);
+	 		strcpy(AA[jj],temp);
+	 		jj=jj-k;
+	 		
+		}
+	 	strcpy(t,AA[jj+k]);
+	 }}
+}
+    void pars (char stro[],int n){
+    	printf("\n%s\n",stro);
+		int i=0;
+    	char ch= stro[0]; //droblenie na slova
+    	int k=0;
+    	char slovo[20];
+    	 
+     //или a4=new char*[n]
+    
+    while (ch != '\0')
+    {
+    		while ( (stro[k-1]==' ') && (stro[k]==' ') && (stro[k] != '\0')&&(k<20)) 
+    			k++;
+	        ch= stro[k];k++;
+			//printf("%c",ch);
+	    	if ((ch != ' ') && (ch != '\0')) 
+			{
+    		slovo[i] = ch;
+	    	i++;
+			}
+			else{
+				if(ch!= '\0')
+				{
+				
+			slovo[i+1] = '\0';	
+			printf("RES %s \n",slovo);
+			A[j]=(char *) calloc(i+1,sizeof(char));
+           // A[j] = new char[i+1];
+            strcpy(A[j], slovo);
+			j++;
+			i=0;
+			for (int i=0; i<20; i++) 
+				slovo[i]='\0';
+			}}
+			
+	}
+	slovo[1+i] = '\0';	
+	printf("RES %s\n",slovo);
+	A[j]=(char *) calloc(i+1,sizeof(char));
+	strcpy(A[j], slovo);
+	j++;
+	i=0;
+return;
 }
 
-/*void Shellsort(char* A, int N)
-{
-        if(strlen(text) == 1){
-        text = new char(1);
-        text[0] = a;}
-        */
-// else{
-/*text= new char[ii];
-text[ii] = ch;
-ii++;
-text=new char[ii];
-text[ii] = '\0';
+int main () {
+	int w,n=20;
+	 A= (char **) malloc(n*sizeof(char *));
+     A = new  pint[n];
+    char ch, sl[200];
+    char text[]="one two three four\n";
+	FILE *pf,*pu; 
+	pf=fopen("A.txt","r"); 
+//fprintf(pf,"%d",5);
+fscanf(pf,"%d",&w);
+ for(int i=0;i<6;i++)
+ {
+ //fputs(text,pf);
+ }
+fclose(pf);
+ 
+pf=fopen("A.txt","r"); //otkritie faila
+printf(" %s\n"," From FILE:  ");
 
-//	printf("before %c      %s ",a, text);
-for (int i=0; i<ii; i++)
-{
-char c = text[i];
-printf("%c", c);
+while ((ch=getc(pf))!=EOF)
+   {
+   fgets(sl, 200, pf);
+   char temp[2];
+   temp[0] = ch;
+   temp[1]='\0';
+   
+   strcpy(temp,  strcat(temp,sl));
+   
+   //printf("%s", temp);
+    pars(temp,n);
 }
 
-void AddToSorted(char ch, char* text)
-{
-text = new char[ii];
-text[ii] = ch;
-ii++;
-text = new char[ii];
-text[ii] = '\0';
-for (int i = 0; i < ii; i++) {
-char c = text[i];
-printf("%c", c);
-}
-printf("\n");
-Shellsort(text, ii + 1);
-for (int i = 0; i < ii; i++) {
-printf("%c", text[i]);
-}
-printf("\n");
-}
-*/
-void f(int i, char*& p)
-{
-    char a;
-    ii++;
-    scanf("%c", &a); //////////////////  scanf("% DDDDD  CCCC   c",&a);
-    if (a == 'q') {
-        p = new char[i + 1];
-        p[i] = a;
-        return;
-    }
-    f(i + 1, p);
-    p[i] = a;
-    printf("%d %c\n", i, a);
-}
-
-int main()
-{
-    //   char* text;
-    // text = new char(1);
-    char* p;
-
-    printf("Vvedite stroki slov. q = zakanchivaet \n");
-
-    f(0, p);
-
-    /*
-   //printf("ghghg");
-     int i,j;
-     char a='1';
-     while (a!= 'q') {
-           scanf("%c",&a);
-           if((a>= 'A') && ( a<='Z') || (a>= 'a') && (a<= 'z'))
-           {
-
-                    AddToSorted(a,text);
-
-             }
+printf("\n This is result array \n");
+    
+	//for(int i=0;i<10;i++){
+	
+     for(int i=0;i<j;i++)  //j - kolvo slov
+     	{
+		  printf("%s \n", A[i]);
      }
-     text[ii+1]='\0';
-       */
-    for (int j = 0; p[j]; j++)
-        printf("%c,", p[j]);
-    printf("\n");
+   //  printf("\n");
+   Shellsort(A,j);
+   
 
-    printf("\n%d = lenth\n", ii);
-    Shellsort(p, ii);
+printf("\n This is result array \n");
+    
+	//for(int i=0;i<10;i++){
+	
+     for(int i=0;i<j;i++)  //j - kolvo slov
+     	{
+		  printf("%s \n", A[i]);
+     }
+	    
+ for(int i=0; i<n; i++) {
+       free(A[i]);
+       }
+     free(A);
 
-    printf("Result is...");
-
-    for (int j = 0; p[j]; j++)
-        printf("%c,", p[j]);
-    printf("\n");
-    system("pause");
+//printf(" %d",w);
+fclose(pf);
+return 0;
 }
