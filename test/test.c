9c8fa77a775329d1698b482d08b3c75dd627db24
listofkin <listofkin@gmail.com>
2020-06-16 22:07:44 +0700

    Добавил тесты для проверки игнорирования символов для русских и латинских букв

--- test/test.c
+++ test/test.c
@@ -4,18 +4,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-CTEST(work_with_string, ignoring_characters)
+CTEST(work_with_string_latin_words, ignoring_characters)
 {
     // Given
-    char* stroka_1 = "шла саша по шоссе и сосала сушку";
-    char* stroka_2 = "apple fortune bananas";
-    char* stroka_3 = "";
-    char* stroka_4 = " apple fortune eggs";
-    char* stroka_5 = " яблоко фортуна яйца";
-    char* stroka_6 = "123 apple ihhi";
-    char* stroka_7 = "яблоко банан груша @";
-    char* stroka_8 = "rice  money";
-    char* stroka_9 = "блокнот  деньги  сыр";
+    char* stroka_1 = "apple fortune bananas";
+    char* stroka_2 = "";
+    char* stroka_3 = " apple fortune eggs";
+    char* stroka_4 = "123 apple ihhi";
+    char* stroka_5 = "rice  money";
 
     // When
     int result_1 = ignoring_characters(stroka_1);
@@ -23,29 +19,47 @@ CTEST(work_with_string, ignoring_characters)
     int result_3 = ignoring_characters(stroka_3);
     int result_4 = ignoring_characters(stroka_4);
     int result_5 = ignoring_characters(stroka_5);
-    int result_6 = ignoring_characters(stroka_6);
-    int result_7 = ignoring_characters(stroka_7);
-    int result_8 = ignoring_characters(stroka_8);
-    int result_9 = ignoring_characters(stroka_9);
 
     // Then
     int expected_1 = 0;
-    int expected_2 = 0;
-    int expected_3 = 1;
-    int expected_4 = 4;
-    int expected_5 = 4;
-    int expected_6 = 2;
-    int expected_7 = 2;
-    int expected_8 = 3;
-    int expected_9 = 3;
+    int expected_2 = 1;
+    int expected_3 = 4;
+    int expected_4 = 2;
+    int expected_5 = 3;
 
     ASSERT_EQUAL(expected_1, result_1);
     ASSERT_EQUAL(expected_2, result_2);
     ASSERT_EQUAL(expected_3, result_3);
     ASSERT_EQUAL(expected_4, result_4);
     ASSERT_EQUAL(expected_5, result_5);
-    ASSERT_EQUAL(expected_6, result_6);
-    ASSERT_EQUAL(expected_7, result_7);
-    ASSERT_EQUAL(expected_8, result_8);
-    ASSERT_EQUAL(expected_9, result_9);
+}
+
+CTEST(work_with_string_russian_words, ignoring_characters)
+{
+    // Given
+    char* stroka_1 = "батон булка хлеб";
+    char* stroka_2 = "";
+    char* stroka_3 = " яйцо ананас банан";
+    char* stroka_4 = "яблоко киви 125";
+    char* stroka_5 = "рис  груша";
+
+    // When
+    int result_1 = ignoring_characters(stroka_1);
+    int result_2 = ignoring_characters(stroka_2);
+    int result_3 = ignoring_characters(stroka_3);
+    int result_4 = ignoring_characters(stroka_4);
+    int result_5 = ignoring_characters(stroka_5);
+
+    // Then
+    int expected_1 = 0;
+    int expected_2 = 1;
+    int expected_3 = 4;
+    int expected_4 = 2;
+    int expected_5 = 3;
+
+    ASSERT_EQUAL(expected_1, result_1);
+    ASSERT_EQUAL(expected_2, result_2);
+    ASSERT_EQUAL(expected_3, result_3);
+    ASSERT_EQUAL(expected_4, result_4);
+    ASSERT_EQUAL(expected_5, result_5);
 }
